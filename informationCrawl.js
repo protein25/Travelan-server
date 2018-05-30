35dd2909376b738534a418b84b31710cc03911a2
choikyubum <bbulbum@gmail.com>
2018-05-30 22:50:59 +0900

    크롤링 최신글 로직 수정

--- informationCrawl.js
+++ informationCrawl.js
@@ -7,7 +7,7 @@ function crawl(targetId, pageNo = 1) {
   let findTarget = false;
 
   return informationCrawler(pageNo)
-    .then((results) => Promise.map(results, (result) => {
+    .then((results) => Promise.each(results, (result) => { // 순서대로 넣기위해 each 사용
       if (result.id[0] === targetId) {
         findTarget = true;
         return;
@@ -29,7 +29,7 @@ function crawl(targetId, pageNo = 1) {
 
 module.exports = () => {
   informations.findOne({
-    order: [['wrtDt', 'desc']],
+    order: [['wrtDt', 'desc'], ['createdAt', 'asc']], // 크롤링 후 먼저 나온 글부터 넣기 때문에 wrtDt가 같은경우 createdAt 으로 먼저 insert된 것이 더 최신글임
   })
   .then((lastInformation) => {
     const targetId = lastInformation.dataId;
